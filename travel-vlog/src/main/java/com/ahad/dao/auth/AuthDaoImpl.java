3ab05f4ee420224c867e1507b0dc5cfa28d50cd0
Ahad <ahad15-12484@diu.edu.bd>
2022-10-05 15:29:25 +0600

    signup information in db

--- travel-vlog/src/main/java/com/ahad/dao/auth/AuthDaoImpl.java
+++ travel-vlog/src/main/java/com/ahad/dao/auth/AuthDaoImpl.java
@@ -1,18 +1,24 @@
 package com.ahad.dao.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.ahad.model.User;
 
 @Repository
-public class AuthDaoImpl implements AuthDao{
+public class AuthDaoImpl implements AuthDao {
+	public static final String INSERT_QUERY = "INSERT INTO `user_table`(`email`, `name`, `password`, `verified`) VALUES (?, ?, ?, ?)";
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@Override
 	public int registerUser(User user) {
-		System.out.println("dao regestering user "+this);
-		if(user.getEmail().equals("a@gmail.com"))
-			return -1;
-		return 1;
+		System.out.println("dao regestering user " + this);
+		try {
+			return jdbcTemplate.update(INSERT_QUERY, user.getEmail(), user.getName(), user.getPassword(), 0);
+		}catch(Exception e) {
+			return 0;
+		}
 	}
-
 }
